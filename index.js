1a5dc9e0e5ff2f0639a9df02e91de0597a267d0e
David Glading <davidjglading@gmail.com>
2020-10-16 16:53:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+
+
+function add(a, b) {
+  20 + 30
+  returns('20 + 30 = 50')
+}
+
+function subtract(a, b) {
+  30 - 20
+  returns('30 - 20 = 10')
+}
+
+function multiply(a, b) {
+  30 * 20
+  returns('30 * 20 = 600')
+}
+
+function divide(a, b) {
+  600 / 30
+  returns('600 / 30 = 20')
+}
+
+var number = 10
+function add5(number) {
+  number += 5
+  returns(10 + 5 = 15)
+}
