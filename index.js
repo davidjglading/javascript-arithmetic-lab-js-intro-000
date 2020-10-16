02fb7e1c722e4aa861391cb0ae17e2409187a0c1
David Glading <davidjglading@gmail.com>
2020-10-16 18:06:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,3 +29,11 @@ function increment(num) {
 function decrement(num) {
   return --num
 }
+
+function makeInt(string) {
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
